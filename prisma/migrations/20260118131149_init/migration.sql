c787e76b476d00246dc3b97dddd31d5eb44bc6c9
elnaxy12 <aryagilang533@gmail.com>
2026-01-18 20:17:29 +0700

    git init

---
+++ prisma/migrations/20260118131149_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `Flight` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `airline` VARCHAR(191) NOT NULL,
+    `from` VARCHAR(191) NOT NULL,
+    `to` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `depart` VARCHAR(191) NOT NULL,
+    `arrive` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `seats` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Booking` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `flightId` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `nik` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Booking` ADD CONSTRAINT `Booking_flightId_fkey` FOREIGN KEY (`flightId`) REFERENCES `Flight`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
